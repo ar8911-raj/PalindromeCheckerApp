655860b52233dcdb7f14b395e3d0718d516b4ad6
Aditya Raj <your-email@example.com>
2026-02-26 16:21:11 +0530

    Aditya Raj - UC4 with word level

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,10 +2,28 @@ package src;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+        String input = "level";
+        char[] charArray = input.toCharArray();
+        int left = 0;
+        int right = charArray.length - 1;
+        boolean isPalindrome = true;
 
-        System.out.println("--- Welcome to Palindrome Checker App ---");
-        System.out.println("Application Name: PalindromeCheckerApp");
-        System.out.println("Application Version: 1.0");
-        System.out.println("-----------------------------------------");
+        while (left < right) {
+            if (Character.toLowerCase(charArray[left]) != Character.toLowerCase(charArray[right])) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        System.out.println("--- UC4: Character Array Based Palindrome Check ---");
+        System.out.println("Word: " + input);
+
+        if (isPalindrome) {
+            System.out.println("Result: It is a palindrome.");
+        } else {
+            System.out.println("Result: It is not a palindrome.");
+        }
     }
 }
