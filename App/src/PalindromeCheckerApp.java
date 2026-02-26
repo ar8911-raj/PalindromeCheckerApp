e08fee5193dc72f3a3f542295692c08cf18ca6c2
Aditya Raj <your-email@example.com>
2026-02-26 16:06:45 +0530

    Aditya Raj - Restoring UC1 code

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -2,21 +2,10 @@ package src;
 
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        String original = "madam";
-        String reversed = "";
 
-        for (int i = original.length() - 1; i >= 0; i--) {
-            reversed = reversed + original.charAt(i);
-        }
-
-        System.out.println("--- UC3: Palindrome Check Using String Reverse ---");
-        System.out.println("Original String: " + original);
-        System.out.println("Reversed String: " + reversed);
-
-        if (original.equalsIgnoreCase(reversed)) {
-            System.out.println("Result: It is a palindrome.");
-        } else {
-            System.out.println("Result: It is not a palindrome.");
-        }
+        System.out.println("--- Welcome to Palindrome Checker App ---");
+        System.out.println("Application Name: PalindromeCheckerApp");
+        System.out.println("Application Version: 1.0");
+        System.out.println("-----------------------------------------");
     }
 }
